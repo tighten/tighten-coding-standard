5f3a47a9d30a7c9824bdff933ba4940888c7e462
Anthony Clark <anthonyclark@gmail.com>
2022-08-05 13:17:42 -0700

    Update laravel config to use new `ECSConfig`

--- config/laravel.php
+++ config/laravel.php
@@ -5,42 +5,30 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
 use PhpCsFixer\Fixer\Operator\NewWithBracesFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/tighten.php');
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->import(__DIR__ . '/tighten.php');
 
     $defaultOptions = include __DIR__ . '/options.php';
 
-    $services = $containerConfigurator->services();
-    $parameters = $containerConfigurator->parameters();
+    $ecsConfig->paths($defaultOptions['paths']);
 
-    $parameters->set(Option::PATHS, $defaultOptions[Option::PATHS]);
-
-    $parameters->set(
-        Option::SKIP,
-        array_merge(
-            $defaultOptions[Option::SKIP],
-            [
-                // Laravel preferences: allow unused imports
-                NoUnusedImportsFixer::class,
-            ]
-        )
-    );
+    // Add additional skip options
+    $ecsConfig->skip(array_merge(
+        $defaultOptions['skip'],
+        [
+            // Laravel preferences: allow unused imports
+            NoUnusedImportsFixer::class,
+        ]
+    ));
 
     // Laravel preferences: no spaces when concatenating
-    $services->set(ConcatSpaceFixer::class);
+    $ecsConfig->rule(ConcatSpaceFixer::class);
 
     // Laravel preferences: no empty braces
-    $services->set(NewWithBracesFixer::class)
-        ->call(
-            'configure',
-            [
-                [
-                    'anonymous_class' => \false,
-                    'named_class' => \false,
-                ],
-            ],
-        );
+    $ecsConfig->ruleWithConfiguration(NewWithBracesFixer::class, [
+        'anonymous_class' => false,
+        'named_class' => false,
+    ]);
 };
